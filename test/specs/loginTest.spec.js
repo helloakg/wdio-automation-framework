23dd6e6369aa36a2ceb1e551ed146c6d6d74297c
Akshay Gouda <helloakshayg@gmail.com>
2024-09-18 09:27:42 +0530

    Initial git commit setup

---
+++ test/specs/loginTest.spec.js
@@ -0,0 +1,13 @@
+describe("Test Case 1",()=>{
+
+    it("test google search page",async()=>{
+        await browser.url("https://www.google.com")
+        await browser.pause(2000)
+        await browser.$("[name='q']").setValue('mobiles')
+        await browser.pause(2000)
+        const title=await browser.getTitle();
+        console.log("Title is: ", title);
+
+})
+
+})
